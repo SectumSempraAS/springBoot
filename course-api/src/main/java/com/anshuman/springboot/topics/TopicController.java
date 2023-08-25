ccc680eea6b178788248ab5afc5700f5947023c9
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-08-24 22:43:15 -0700

    feat: business services

--- course-api/src/main/java/com/anshuman/springboot/topics/TopicController.java
+++ course-api/src/main/java/com/anshuman/springboot/topics/TopicController.java
@@ -1,20 +1,19 @@
 package com.anshuman.springboot.topics;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
     
     @RequestMapping("/topic")
     public List<Topic> giveTopic() {
-        return Arrays.asList(
-            new Topic("11", "Anil", "in delhi"),
-            new Topic("12", "anshuman", "in bengaluru"),
-            new Topic("13", "Anumit", "in New york")
-            );
+        return topicService.getAllTopics();
     }
 }
---
+++ course-api/src/main/java/com/anshuman/springboot/topics/TopicService.java
@@ -0,0 +1,22 @@
+package com.anshuman.springboot.topics;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+    private List<Topic> topicLists = Arrays.asList(
+            new Topic("11", "Anil1", "in delhi"),
+            new Topic("12", "anshuman", "in bengaluru"),
+            new Topic("13", "Anumit", "in New york")
+            );
+
+    public List<Topic> getAllTopics() {
+        return topicLists;
+    }
+}
+
+// a business service (singleton) created which will help spring register a instance 
+// of this class which can be used by controllers or other services.
