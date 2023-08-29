0313710096c29d618194e24485bfc947ede47fc2
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-08-29 10:38:20 +0530

    chore: remove unnecessary imports

--- course-api-data/src/main/java/com/anshuman/springboot/courseapidata/topics/TopicService.java
+++ course-api-data/src/main/java/com/anshuman/springboot/courseapidata/topics/TopicService.java
@@ -13,12 +13,6 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-    private List<Topic> topicLists = new ArrayList<>(Arrays.asList(
-            new Topic("11", "Anil1", "in delhi"),
-            new Topic("12", "anshuman", "in bengaluru"),
-            new Topic("13", "Anumit", "in New york")
-            ));
-
     public List<Topic> getAllTopics() {
         List<Topic> topics = new ArrayList<>();
         topicRepository.findAll().forEach(topics::add);
@@ -26,7 +20,8 @@ public class TopicService {
     }
 
     public Topic getTopic(String id) {
-        return topicLists.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        //return topicLists.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
@@ -34,17 +29,11 @@ public class TopicService {
     }
 
     public void updateTopic(Topic newTopic, String id) {
-        for (int i = 0 ; i < topicLists.size() ; i++) {
-            Topic t = topicLists.get(i);
-            if (t.getId().equals(id)) {
-                topicLists.set(i, newTopic);
-                return;
-            }
-        }
+        topicRepository.save(newTopic);
     }
 
     public void deleteTopic(String id) {
-        topicLists.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
 
